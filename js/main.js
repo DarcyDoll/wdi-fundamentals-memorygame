0ae35e253ac8c2741b5b35c3ebec5d8f6c2d456a
Darcy Martin <darcydoll@Darcys-MacBook-Pro.local>
2018-02-05 15:38:53 -0800

    added alert to notify of winning or losing message

--- js/main.js
+++ js/main.js
@@ -1,7 +1,14 @@
-console.log("Up amd running!");
-var cardOne = "queen";
-var cardTwo = "king";
-var cardThree = "queen";
-var cardFour = "king";
+console.log("Up and running!");
+var cards = ["queen", "queen", "king", "king"];
+var cardsInPlay = [];
+var cardOne = cards[0];
+var cardTwo = cards[2];
+cardsInPlay.push("queen");
 console.log("User flipped " + cardOne);
-console.log("User flipped " + cardThree);
+cardsInPlay.push("king");
+console.log("User flipped " + cardTwo);
+if (cardsInPlay[0] === cardsInPlay[2]) {
+alert("You found a match!");
+} else {
+  alert("Sorry, try again.");
+}
